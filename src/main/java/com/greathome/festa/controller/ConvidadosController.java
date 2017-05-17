3187143c8fbd3e1cbb066d2075d85056c537a68b
leandro <leandro@localhost.localdomain>
2017-05-16 23:44:30 -0300

    Atualizado classe ConvidadosController method excluir

--- src/main/java/com/greathome/festa/controller/ConvidadosController.java
+++ src/main/java/com/greathome/festa/controller/ConvidadosController.java
@@ -1,19 +1,19 @@
 package com.greathome.festa.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.greathome.festa.model.Convidado;
 import com.greathome.festa.repository.Convidados;
+
+import ch.qos.logback.classic.Logger;
 
 
 @Controller
@@ -21,6 +21,8 @@ public class ConvidadosController {
 
 	@Autowired//interface Convidados
 	private Convidados convidados;
+	
+	private Logger logger;
 	
 	private ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 	
@@ -38,10 +40,10 @@ public class ConvidadosController {
 		return "redirect:/convidados";
 	}
 	
-	//@GetMapping("/convidados")
-	@RequestMapping(value="convidado", method=RequestMethod.DELETE)
-	public String excluir(@RequestParam Long id) {
-		this.convidados.delete(id);
+	@RequestMapping(value="/convidados/{id}", method=RequestMethod.GET)
+	public String excluir(Convidado convidado) {	
+		this.convidados.delete(convidado.getId());
+		//modelAndView.addObject("convidados",convidados.findAll());
 		return "redirect:/convidados";
 	}
 }
--- src/main/java/com/greathome/festa/repository/Convidados.java
+++ src/main/java/com/greathome/festa/repository/Convidados.java
@@ -1,9 +1,11 @@
 package com.greathome.festa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.greathome.festa.model.Convidado;
 
+@Repository
 public interface Convidados extends JpaRepository<Convidado, Long>{
 
 }
